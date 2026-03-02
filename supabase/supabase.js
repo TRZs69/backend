5b150bff807ee5fdaf43158f54dbf97b22bae480
BuffDoge <153471156+TRZs69@users.noreply.github.com>
2026-03-02 15:59:11 +0700

    fix: update supabase configurations and urls

--- supabase/supabase.js
+++ supabase/supabase.js
@@ -1,6 +1,6 @@
 const { createClient } = require('@supabase/supabase-js');
 
-const fallbackUrl = 'https://vvivfqnqxnpfpijrvkkb.supabase.co';
+const fallbackUrl = 'https://itarozdimxukkhwxruti.supabase.co';
 const fallbackAnonKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ2aXZmcW5xeG5wZnBpanJ2a2tiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg2MTQxMjEsImV4cCI6MjA3NDE5MDEyMX0.VwNktSJnyCuvBHEEMw4hv4wsHm7wT1MxS6foqR2i4Nk';
 
