01edeeb35230a03c54fed54f6adc184ad688a49f
BuffDoge <153471156+TRZs69@users.noreply.github.com>
2026-03-04 14:36:29 +0700

    fix(prisma): restrict serverless db connection limits, fix 500 in course service

--- src/prismaClient.js
+++ src/prismaClient.js
@@ -1,5 +1,13 @@
 const { PrismaClient } = require('@prisma/client');
 
+if (process.env.DATABASE_URL) {
+	if (process.env.DATABASE_URL.includes("connection_limit=")) {
+		process.env.DATABASE_URL = process.env.DATABASE_URL.replace(/connection_limit=\d+/, 'connection_limit=3');
+	} else {
+		process.env.DATABASE_URL += (process.env.DATABASE_URL.includes("?") ? "&" : "?") + "connection_limit=3";
+	}
+}
+
 const globalForPrisma = globalThis;
 
 const prisma = globalForPrisma.prisma || new PrismaClient();
--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -2,7 +2,7 @@ const prisma = require('../prismaClient');
 
 exports.getAllCourses = async () => {
     try {
-        const courses = await prisma.course.findMany(); 
+        const courses = await prisma.course.findMany();
         return courses;
     } catch (error) {
         throw new Error(error.message);
@@ -27,49 +27,45 @@ exports.createCourse = async (newData) => {
         const newCourse = await prisma.course.create({
             data: newData,
         });
-        
+
         return newCourse;
     } catch (error) {
         throw new Error(error.message);
     }
 };
 
-exports.updateCourse = async(id, updateData) => {
+exports.updateCourse = async (id, updateData) => {
     try {
         const course = await prisma.course.update({
-            where: { id },      
-            data: updateData,     
+            where: { id },
+            data: updateData,
         });
-        return course;  
+        return course;
     } catch (error) {
-        throw new Error(error.message);  
+        throw new Error(error.message);
     }
 }
 
-exports.deleteCourse = async(id) => {
+exports.deleteCourse = async (id) => {
     try {
         await prisma.course.delete({
             where: { id },
         });
         return "Success deleting course";
     } catch (error) {
-        throw new Error('Error deleting course: ' + error.message); 
+        throw new Error('Error deleting course: ' + error.message);
     }
 }
 
 // Special Services
 
-exports.getChapterByCourse = async(id) => {
+exports.getChapterByCourse = async (id) => {
     try {
         const chapters = await prisma.chapter.findMany({
             where: {
                 courseId: parseInt(id)
             }
         });
-
-        if (!chapters.length) {
-            throw new Error(`No chapter found for course with id ${id}`);
-        }
 
         return chapters;
     } catch (error) {
