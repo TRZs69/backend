92b6026e0fceb379f9d9a7bb526a623620478f4c
adtyarhz <149128711+Adtyarhz@users.noreply.github.com>
2026-04-08 22:41:01 +0700

    Update prismaClient.js

--- src/prismaClient.js
+++ src/prismaClient.js
@@ -2,10 +2,15 @@ const { PrismaClient } = require('@prisma/client');
 
 if (process.env.DATABASE_URL) {
 	if (process.env.DATABASE_URL.includes("connection_limit=")) {
-		process.env.DATABASE_URL = process.env.DATABASE_URL.replace(/connection_limit=\d+/, 'connection_limit=50');
+		process.env.DATABASE_URL = process.env.DATABASE_URL.replace(/connection_limit=\d+/, 'connection_limit=20');
 	} else {
-		process.env.DATABASE_URL += (process.env.DATABASE_URL.includes("?") ? "&" : "?") + "connection_limit=50";
+		process.env.DATABASE_URL += (process.env.DATABASE_URL.includes("?") ? "&" : "?") + "connection_limit=20";
 	}
+    
+    // Antrian request di Prisma Pool agar saat diakses banyak siswa bersamaan tidak langsung ditolak
+    if (!process.env.DATABASE_URL.includes("pool_timeout=")) {
+        process.env.DATABASE_URL += "&pool_timeout=60";
+    }
 }
 
 const globalForPrisma = globalThis;
