ded2b21e4e9795b703eab9cb6a5ced1bd36a3be2
BuffDoge <153471156+TRZs69@users.noreply.github.com>
2026-04-09 09:27:50 +0700

    connection limit and pool adjustment

--- src/prismaClient.js
+++ src/prismaClient.js
@@ -2,14 +2,14 @@ const { PrismaClient } = require('@prisma/client');
 
 if (process.env.DATABASE_URL) {
 	if (process.env.DATABASE_URL.includes("connection_limit=")) {
-		process.env.DATABASE_URL = process.env.DATABASE_URL.replace(/connection_limit=\d+/, 'connection_limit=65');
+		process.env.DATABASE_URL = process.env.DATABASE_URL.replace(/connection_limit=\d+/, 'connection_limit=25');
 	} else {
-		process.env.DATABASE_URL += (process.env.DATABASE_URL.includes("?") ? "&" : "?") + "connection_limit=65";
+		process.env.DATABASE_URL += (process.env.DATABASE_URL.includes("?") ? "&" : "?") + "connection_limit=25";
 	}
-    
+
     // Antrian request di Prisma Pool agar saat diakses banyak siswa bersamaan tidak langsung ditolak
     if (!process.env.DATABASE_URL.includes("pool_timeout=")) {
-        process.env.DATABASE_URL += "&pool_timeout=60";
+        process.env.DATABASE_URL += "&pool_timeout=15";
     }
 }
 
