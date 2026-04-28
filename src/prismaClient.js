d116af29ef9a5d8d67a71dcf6f44ad0421614962
BuffDoge <153471156+TRZs69@users.noreply.github.com>
2026-04-28 21:21:13 +0700

    connection limit and pool timeout set from .env

--- src/prismaClient.js
+++ src/prismaClient.js
@@ -1,15 +1,23 @@
 const { PrismaClient } = require('@prisma/client');
 
 if (process.env.DATABASE_URL) {
+	const connectionLimit = process.env.DB_CONNECTION_LIMIT;
+	const poolTimeout = process.env.DB_POOL_TIMEOUT;
+
 	if (process.env.DATABASE_URL.includes("connection_limit=")) {
-		process.env.DATABASE_URL = process.env.DATABASE_URL.replace(/connection_limit=\d+/, 'connection_limit=40');
+		process.env.DATABASE_URL = process.env.DATABASE_URL.replace(
+			/connection_limit=\d+/,
+			`connection_limit=${connectionLimit}`
+		);
 	} else {
-		process.env.DATABASE_URL += (process.env.DATABASE_URL.includes("?") ? "&" : "?") + "connection_limit=40";
+		process.env.DATABASE_URL +=
+			(process.env.DATABASE_URL.includes("?") ? "&" : "?") +
+			`connection_limit=${connectionLimit}`;
 	}
 
-    if (!process.env.DATABASE_URL.includes("pool_timeout=")) {
-        process.env.DATABASE_URL += "&pool_timeout=30";
-    }
+	if (!process.env.DATABASE_URL.includes("pool_timeout=")) {
+		process.env.DATABASE_URL += `&pool_timeout=${poolTimeout}`;
+	}
 }
 
 const globalForPrisma = globalThis;
