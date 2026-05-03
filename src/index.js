0603af96f0634304d9245c45841020f8f717014a
Ralphael Siahaan <siahaan.ralphael@gmail.com>
2026-05-04 01:07:34 +0700

    added patch method for supabase handling

--- src/index.js
+++ src/index.js
@@ -49,7 +49,7 @@ app.use(
       }
       return callback(new Error("Not allowed by CORS"));
     },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     credentials: true,
   }),
 );
